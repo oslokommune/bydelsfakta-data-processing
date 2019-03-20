83231f5407b99bb74a589b872c85b263f57bb835
tordsk <tord.kloster@knowit.no>
2019-03-20 14:58:24 +0100

    use new common functionality for boligpriser

--- boligpriser.py
+++ boligpriser.py
@@ -1,128 +1,52 @@
-import json
-
-import pandas as pd
-import numpy as np
-import re
-import pandas_layer as pl
-import bydelsfakta_layer as bl
-import logging
-
-pd.set_option('display.max_colwidth', -1)
-pd.set_option('display.max_columns', 30)
-
-logging.basicConfig(level=logging.INFO)
+import common.aws
+import common.transform
+import common.util
 
 
 def handler(event, context):
-    logging.info(event)
-    bucket, key, groups = pl.match_s3_key(event)
-    df = read_csv(bucket, key)
-    historic = create_dataset(df)
-    # TODO: Since we are missing aggregated date for districts from 2018, we cannot use max() of this row yet
-    status = create_dataset(df[df.År == 2017])
+    """ Assuming we recieve a complete s3 key"""
+    s3_key = event['keys']['boligpriser-urFqK']
+    bucket = event['bucket']
+    start(bucket, s3_key)
+    return "OK"
 
-    unfinished_output_key = 'processed/green/{dataset}/{version}/{edition}/'.format(
-        dataset=groups['dataset'],
-        version=groups['version'],
-        edition=groups['edition']
+
+def start(bucket, key):
+    original = common.aws.read_from_s3(
+            s3_key=key,
+            value_column="kvmpris",
+            date_column="År"
     )
+    original = common.transform.add_district_id(original, "Oslo-Bydelsnavn")
+    original = original.drop(
+        columns=['antall omsatte blokkleieligheter', 'Oslo-Bydelsnavn', 'Delbydelnummer', 'Delbydelsnavn'])
 
-    write_dataset(historic, 'historic', bucket, unfinished_output_key)
-    write_dataset(status, 'status', bucket, unfinished_output_key)
+    status = common.transform.status(original)
+    historic = common.transform.historic(original)
 
-def ensure_alphanumeric(filename):
-    o_regex = re.compile('[Øø]')
-    a_regex = re.compile('[Åå]')
-    ae_regex = re.compile('[Ææ]')
-    u_regex = re.compile('[Üü]')
-    default_regex = re.compile('[^a-zA-Z0-9]')
-
-    filename = o_regex.sub("o", filename)
-    filename = a_regex.sub("a", filename)
-    filename = ae_regex.sub("ae", filename)
-    filename = u_regex.sub("u", filename)
-    filename = default_regex.sub('_', filename)
-    return filename
-
-def write_dataset(df, suffix, bucket, unfinished_output_key):
-    logging.info('Writing ' + suffix)
-    for current_district, subdistrict in df:
-
-        s3_key = unfinished_output_key + "{current_district}-{suffix}.json".format(current_district=ensure_alphanumeric(current_district),
-                                                                                   suffix=suffix)
-
-        district = bl.generate_metadata(current_district,
-                                        heading="Gjennomsnittpris (kr) pr kvm for blokkleilighet",
-                                        series=None,
-                                        data=subdistrict
-                                        )
-        pl.write_json_to_s3(bucket=bucket, key=s3_key, data=json.dumps(district, ensure_ascii=False))
-    logging.info('Complete ' + suffix)
+    create_ds(bucket, "boligpriser_historic-4owcY", *historic)
+    create_ds(bucket, "boligpriser_status-pD7ZV", *status)
 
 
-def read_csv(bucket, key):
-    df_source = pl.read_csv_from_s3(bucket, key, separator=';')
-    df_source = df_source.drop(columns=['antall omsatte blokkleieligheter', 'Delbydelnummer']) \
-        .rename(columns={'kvmpris': 'value', 'Oslo-Bydelsnavn': 'bydelsnavn', 'Delbydelsnavn': 'geography'})
-    return df_source
+def create_ds(bucket, dataset_id, df):
+    heading = "Gjennomsnittpris (kr) pr kvm for blokkleilighet"
+    series = [{'heading': 'Gjennomsnittpris (kr) pr kvm for blokkleilighet', 'subheading': ''}]
 
+    # To json : convert df to list of json objects
+    json = [{'x': """to_json(df)"""}]
 
-def fix_district_prefix(line):
-    if line.startswith("Bydel "):
-        return fix_district_prefix(line[6:])
-    elif line.startswith("St "):
-        return re.sub('^%s' % "St ", "St.", line)
-    else:
-        return line
-
-
-def create_district(district, group, oslo_in_all):
-    data_list = [] + oslo_in_all
-    for subdistrict, values in group.groupby(['geography']):
-        data = {'geography': subdistrict, 'values': values.drop(columns=['geography', 'bydelsnavn']).to_dict('records')}
-        if subdistrict == 'AGGREGATED':
-            data['geography'] = district
-            if district == 'Oslo i alt':
-                data['totalRow'] = True
-            else:
-                data['aggRow'] = True
-        data_list.append(data)
-    return district, data_list
-
-
-def create_oslo(df, level):
-    data_list = []
-    for name, value in df.groupby(['geography']):
-        data = {'geography': name, 'values': value.drop(columns=['geography']).to_dict('records')}
-        if name == 'Oslo i alt':
-            data[level] = True
-        data_list.append(data)
-    return data_list
-
-
-def create_dataset(df_source):
-    df_source['bydelsnavn'] = df_source['bydelsnavn'].apply(lambda x: fix_district_prefix(x))
-    df_source['geography'] = df_source['geography'].fillna('AGGREGATED')
-
-    oslo_in_all = df_source[(df_source['bydelsnavn'] == 'Oslo i alt')]
-    oslo_in_all = create_oslo(oslo_in_all.drop(columns=['geography']).rename(columns={'bydelsnavn': 'geography'}),
-                              'totalRow')
-
-    total_oslo = df_source[(df_source['geography'] == 'AGGREGATED')]
-    total_oslo = total_oslo.drop(columns=['geography']).rename(columns={'bydelsnavn': 'geography'})
-    total_oslo = "Oslo i alt", create_oslo(total_oslo, "avgRow")
-
-    df_source = df_source.groupby(['bydelsnavn'])
-    ds_list = [create_district(district, group, oslo_in_all) for district, group in df_source]
-    return ds_list + [total_oslo]
+    common.aws.write_to_intermediate(
+            output_key=f"intermediate/green/{dataset_id}/version=1/edition=???/",
+            heading=heading,
+            series=series,
+            output_list=json)
+    return
 
 
 if __name__ == '__main__':
-    event = {'Records': [
-        {'s3': {
-            'bucket': {'name': 'test-bucket'},
-            'object': {'key': 'raw/green/datasetid/version/1030/distribution.csv'}
-        }
-        }
-    ]}
-    handler(event, {})
+    handler(
+            {'bucket': 'ok-origo-dataplatform-dev',
+             'keys': {
+                 'boligpriser-urFqK': 'raw/green/boligpriser-urFqK/version=1-NrHiJxwf/edition=EDITION-EbL7P/Boligpriser.csv'}
+             }
+            , {})
---
+++ common/util.py
@@ -0,0 +1,54 @@
+_districts_id = {
+    "01": 'Bydel Gamle Oslo',
+    "02": "Bydel Grünerløkka",
+    "03": "Bydel Sagene",
+    "04": "Bydel St. Hanshaugen",
+    "05": "Bydel Frogner",
+    "06": "Bydel Ullern",
+    "07": "Bydel Vestre Aker",
+    "08": "Bydel Nordre Aker",
+    "09": "Bydel Bjerke",
+    "10": "Bydel Grorud",
+    "11": "Bydel Stovner",
+    "12": "Bydel Alna",
+    "13": "Bydel Østensjø",
+    "14": "Bydel Nordstrand",
+    "15": "Byel Søndre Nordstrand",
+    "16": "Sentrum",
+    "17": "Marka",
+    "99": "Uten registrert adresse"
+}
+
+_districts_name = {
+    "Bydel Gamle Oslo": "01",
+    "Bydel Grünerløkka": "02",
+    "Bydel Sagene": "03",
+    "Bydel St.Hanshaugen": "04",
+    "Bydel St. Hanshaugen": "04",
+    "Bydel St Hanshaugen": "04",
+    "Bydel Frogner": "05",
+    "Bydel Ullern": "06",
+    "Bydel Vestre Aker": "07",
+    "Bydel Nordre Aker": "08",
+    "Bydel Bjerke": "09",
+    "Bydel Grorud": "10",
+    "Bydel Stovner": "11",
+    "Bydel Alna": "12",
+    "Bydel Østensjø": "13",
+    "Bydel Nordstrand": "14",
+    "Bydel Søndre Nordstrand": "15",
+    "Sentrum": "16",
+    "Marka": "17",
+    "Uten registrert adresse": "99",
+    "Oslo i alt": "00"
+}
+
+
+def get_district_id(name):
+    if not name.startswith("Bydel ") and name not in ['Marka', 'Sentrum', 'Oslo i alt', 'Uten registrert adresse']:
+        name = f"Bydel {name}"
+    return _districts_name[name]
+
+
+def get_district_name(id):
+    return _districts_id[id]
