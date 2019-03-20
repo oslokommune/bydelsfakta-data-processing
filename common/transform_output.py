a0aea04ce957e8e64c3223674661d6120375973a
oyvind.nygard@knowit.no <oyvind.nygard@knowit.no>
2019-03-20 16:53:27 +0100

    Added common funcionality for transforming dataframe to output list

---
+++ common/transform_output.py
@@ -0,0 +1,160 @@
+
+"""
+generate_output_list:
+    Arguments:
+    * df: input dataframe
+        Input dataframe must have the following columns:
+        | sub_district | district | date | data_point1 - n (values) |
+
+        sub_district: 4-character code for sub_district
+        district: 2-digit code for district
+        date: year
+        data_point1- n: One column for each data_point
+
+    * template: template to be used for generating output json
+        valid values: 'a', 'c', 'i' so far...
+
+    * data_points: List of column labels for the values that will be aggregated
+        The labels in this list must correspond to existing column labels in the input dataframe.
+        If the dataframe also contains ratio columns('data_point_ration'), these should not be in the data_points parameter list
+"""
+
+
+def generate_output_list(df, template, data_points):
+    district_list =  list(
+        filter(lambda x: x not in ['00', '16', '17', '99'], df['district'].unique())
+        )
+    output_list = []
+    oslo_total = [district_time_series(df, '00', template, data_points)]
+    for district in district_list:
+        output_list.append({
+            'district': district,
+            'template': template,
+            'data': district_time_series_list(df, district, template, data_points)
+        })
+        oslo_total.append(district_time_series(df, district, template, data_points))
+    output_list.append({'district': '00', 'template': template, 'data': oslo_total})
+
+    return output_list
+
+def district_time_series_list(df, district, template, data_points):
+    time_series = [district_time_series(df, '00', template, data_points), district_time_series(df, district, template, data_points)]
+    sub_districts = list(
+        filter(lambda x: x is not None, df[df['district'] == district]['sub_district'].unique())
+        )
+    for sub_district in sub_districts:
+        time_series.append(sub_district_time_series(df, sub_district, template, data_points))
+    return time_series
+
+
+def district_time_series(df, district, template, data_points):
+    district_df = df[df['district'] == district]
+    district_df = district_df[district_df['sub_district'].isnull()]
+    return df_to_template(district, district_df, template, data_points)
+
+
+def sub_district_time_series(df, sub_district, template, data_points):
+    sub_district_df = df[df['sub_district'] == sub_district]
+    return df_to_template(sub_district, sub_district_df, template, data_points)
+
+
+def df_to_template(geography, df, template, data_points):
+    if template.lower() == 'a':
+        return df_to_template_a(geography, df, data_points)
+    elif template.lower() == 'c':
+        return df_to_template_c(geography, df, data_points)
+    elif template.lower() == 'i':
+        return df_to_template_i(geography, df, data_points)
+    else:
+        raise Exception(f'Template {template} does not exist')
+
+
+def df_to_template_a(geography, df, data_points):
+    obj_a = {
+        'linkTo': False,
+        'geography': geography,
+        'values': []
+    }
+    series = {}
+    for values in df.to_dict('r'):
+        for data_point in data_points:
+            series[data_point] = value_entry_a(values, data_point)
+    [obj_a['values'].append(series[data_point]) for data_point in data_points]
+    return obj_a
+
+
+def df_to_template_c(geography, df, data_points):
+    obj_c = {
+        'geography': geography,
+        'values': [],
+        'avgRow': False,
+        'totalRow': False
+    }
+    time_series = list_to_time_series(data_points)
+    for values in df.to_dict('r'):
+        [time_series[data_point].append(value_entry_c(values, data_point))
+         for data_point in data_points]
+
+
+    [obj_c['values'].append(time_series[data_point]) for data_point in data_points]
+    return obj_c
+
+
+def df_to_template_i(geography, df, data_points):
+    obj_i = {
+        'geography': geography,
+        'values': []
+    }
+    series = {}
+    for values in df.to_dict('r'):
+        for data_point in data_points:
+            series[data_point] = value_entry_a(values, data_point)
+
+    [obj_i['values'].append(series[data_point]) for data_point in data_points]
+    return obj_i
+
+
+def list_to_time_series(data_points):
+    d = {}
+    for data_point in data_points:
+        d[data_point] = []
+    return d
+
+
+def value_entry_a(values, data_point):
+    if f'{data_point}_ratio' in values:
+        return {
+            'value': values[data_point],
+            'ratio': values[f'{data_point}_ratio']
+        }
+    else:
+        return {
+            'value': values[data_point]
+        }
+
+
+def value_entry_c(values, data_point):
+    if f'{data_point}_ratio' in values:
+        return {
+            'value': values[data_point],
+            'ratio': values[f'{data_point}_ratio'],
+            'date': values['date']
+        }
+    else:
+        return {
+            'value': values[data_point],
+            'date': values['date']
+        }
+
+
+def apply_ratio_colums(df, columns):
+    for col in columns:
+        df[f'{col}_ratio']=compute_ratio(df, columns, col)
+    return df
+
+
+def compute_ratio(df, columns, target):
+    col_sum = 0.0
+    for col in columns:
+        col_sum += df[col]
+    return df[target]/col_sum
---
+++ tests/transform_output_test.py
@@ -0,0 +1,157 @@
+import unittest
+
+import common.transform_output as transform
+import tests.transform_output_test_data as test_data
+import pandas as pd
+
+def empty_str_to_none(s):
+    if len(s) == 0:
+        return None
+    else:
+        return s
+
+test_df = pd.read_csv(f'tests/transform_output_test_input.csv', sep=';', converters={
+        'sub_district': lambda x: empty_str_to_none(str(x)),
+        'district': lambda x: str(x)
+    })
+
+test_df_latest = test_df[test_df['date'] == 2018]
+
+class Tester(unittest.TestCase):
+
+    def test_value_entry_c(self):
+        values_no_ratio = {'d1': 1.0, 'date': 2018}
+        values_with_ratio = {'d1': 1.0, 'd1_ratio': 0.5, 'date': 2018}
+
+        self.assertDictEqual(transform.value_entry_c(values_no_ratio, 'd1'),
+                             {'value': 1.0, 'date': 2018})
+        self.assertDictEqual(transform.value_entry_c(values_with_ratio, 'd1'),
+                             {'value': 1.0, 'ratio': 0.5, 'date': 2018})
+
+    def test_value_entry_a(self):
+        values_no_ratio = {'d1': 1.0, 'date': 2018}
+        values_with_ratio = {'d1': 1.0, 'd1_ratio': 0.5, 'date': 2018}
+
+        self.assertDictEqual(transform.value_entry_a(values_no_ratio, 'd1'),
+                             {'value': 1.0})
+        self.assertDictEqual(transform.value_entry_a(values_with_ratio, 'd1'),
+                             {'value': 1.0, 'ratio': 0.5})
+
+    def test_list_to_time_series(self):
+        data_points = ['d1', 'd2', 'd3']
+        self.assertDictEqual(transform.list_to_time_series(data_points),
+                             {'d1':[],'d2':[],'d3':[]})
+
+    def test_df_to_template_a(self):
+        geography = '0301010101'
+        input_df = test_df_latest[test_df_latest['sub_district'] == geography]
+        data_points = ['d1', 'd2']
+        output = transform.df_to_template_a(geography, input_df, data_points)
+        expected = {
+            'geography': '0301010101',
+            'linkTo': False,
+            'values': [
+                {'value': 'd1_0101_2018', 'ratio': 'd1_0101_2018_ratio'},
+                {'value': 'd2_0101_2018', 'ratio': 'd2_0101_2018_ratio'}]
+        }
+        self.assertDictEqual(output, expected)
+
+    def test_df_to_template_c(self):
+        geography = '0301010101'
+        input_df = test_df[test_df['sub_district'] == geography]
+        data_points = ['d1', 'd2']
+        output = transform.df_to_template_c(geography, input_df, data_points)
+        expected = {
+            'geography': '0301010101',
+            'avgRow': False,
+            'totalRow': False,
+            'values': [
+                [
+                    {'value': 'd1_0101_2017', 'ratio': 'd1_0101_2017_ratio', 'date': 2017},
+                    {'value': 'd1_0101_2018', 'ratio': 'd1_0101_2018_ratio', 'date': 2018}
+                ],
+                [
+                    {'value': 'd2_0101_2017', 'ratio': 'd2_0101_2017_ratio', 'date': 2017},
+                    {'value': 'd2_0101_2018', 'ratio': 'd2_0101_2018_ratio', 'date': 2018}
+                ]
+            ]
+        }
+        self.assertDictEqual(output, expected)
+
+
+    def test_df_to_template_i(self):
+        geography = '0301010101'
+        input_df = test_df_latest[test_df_latest['sub_district'] == geography]
+        data_points = ['d1', 'd2']
+        output = transform.df_to_template_i(geography, input_df, data_points)
+        expected = {
+            'geography': '0301010101',
+            'values': [
+                {'value': 'd1_0101_2018', 'ratio': 'd1_0101_2018_ratio'},
+                {'value': 'd2_0101_2018', 'ratio': 'd2_0101_2018_ratio'}]
+        }
+        self.assertDictEqual(output, expected)
+
+    def test_sub_district_time_series(self):
+        sub_district = '0301010101'
+        template = 'c'
+        data_points = ['d1', 'd2']
+        output = transform.sub_district_time_series(test_df, sub_district, template, data_points)
+        expected = {
+            'geography': '0301010101',
+            'avgRow': False,
+            'totalRow': False,
+            'values': [
+                [
+                    {'value': 'd1_0101_2017', 'ratio': 'd1_0101_2017_ratio', 'date': 2017},
+                    {'value': 'd1_0101_2018', 'ratio': 'd1_0101_2018_ratio', 'date': 2018}
+                ],
+                [
+                    {'value': 'd2_0101_2017', 'ratio': 'd2_0101_2017_ratio', 'date': 2017},
+                    {'value': 'd2_0101_2018', 'ratio': 'd2_0101_2018_ratio', 'date': 2018}
+                ]
+            ]
+        }
+        self.assertDictEqual(output, expected)
+
+    def test_district_time_series(self):
+        district = '01'
+        template = 'c'
+        data_points = ['d1', 'd2']
+        output = transform.district_time_series(test_df, district, template, data_points)
+        expected = {
+            'geography': '01',
+            'avgRow': False,
+            'totalRow': False,
+            'values': [
+                [
+                    {'value': 'd1_01_2017', 'ratio': 'd1_01_2017_ratio', 'date': 2017},
+                    {'value': 'd1_01_2018', 'ratio': 'd1_01_2018_ratio', 'date': 2018}
+                ],
+                [
+                    {'value': 'd2_01_2017', 'ratio': 'd2_01_2017_ratio', 'date': 2017},
+                    {'value': 'd2_01_2018', 'ratio': 'd2_01_2018_ratio', 'date': 2018}
+                ]
+            ]
+        }
+        self.assertDictEqual(output, expected)
+
+    def test_district_time_series_list(self):
+        district = '01'
+        template = 'c'
+        data_points = ['d1', 'd2']
+        output = transform.district_time_series_list(test_df, district, template, data_points)
+        expected = test_data.district_01_time_series_list
+        self.assertListEqual(output, expected)
+
+
+    def test_generate_output_list(self):
+        template = 'c'
+        data_points = ['d1', 'd2']
+        output = transform.generate_output_list(test_df, template, data_points)
+        expected = test_data.output_list
+        self.assertListEqual(output, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
