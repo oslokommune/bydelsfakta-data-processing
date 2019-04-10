8d2c72954b2eb1e4cc2e4f7023882bd24fcd6b9b
oyvind.nygard@knowit.no <oyvind.nygard@knowit.no>
2019-04-10 16:59:17 +0200

    Generating historic and status

--- functions/landbakgrunn_innvandringsgrupper.py
+++ functions/landbakgrunn_innvandringsgrupper.py
@@ -5,19 +5,19 @@ import pandas as pd
 import common.aws as common_aws
 import common.transform as transform
 import common.aggregate_dfs as aggregator
+import common.util as util
 from common.transform_output import generate_output_list
-import json
 
 os.environ['METADATA_API_URL'] = ''
 
 s3_bucket = 'ok-origo-dataplatform-dev'
 
-historic_dataset_id = ''
-historic_version_id = ''
-historic_edition_id = ''
-status_dataset_id = ''
-status_version_id = ''
-status_edition_id = ''
+historic_dataset_id = 'Landbakgrunn-innvandringsgrupp-87zEq'
+historic_version_id = '1-JNRhrxaH'
+historic_edition_id = 'EDITION-Zkwpy'
+status_dataset_id = 'Landbakgrunn-innvandringsgrupp-cLcKm'
+status_version_id = '1-jwbJpksw'
+status_edition_id = 'EDITION-FbQet'
 
 pd.set_option('display.max_rows', 1000)
 
@@ -30,65 +30,80 @@ def handle(event, context):
 
 
 def start(landbakgrunn_key, befolkning_key):
-    landbakgrunn_df = common_aws.read_from_s3(
+    landbakgrunn_raw = common_aws.read_from_s3(
         s3_key=landbakgrunn_key,
         date_column='År'
     )
+    landbakgrunn_raw = pivot_table(landbakgrunn_raw, 'Innvandringskategori', 'Antall personer')
 
     befolkning_raw = common_aws.read_from_s3(
         s3_key=befolkning_key,
         date_column='År'
     )
 
-    ''' Antall personer | date | Bydel | Innvandringskategori | Landbakgrunn'''
-    landbakgrunn_df = pivot_table(landbakgrunn_df, 'Innvandringskategori', 'Antall personer')#landbakgrunn_df[landbakgrunn_df['Innvandringskategori'] == 'Innvandrer']
-    data_points = ['Innvandrer', 'Norskfødt med innvandrerforeldre']
-    landbakgrunn_df['total'] = landbakgrunn_df[data_points].sum(axis=1)
-    data_points.append('total')
+    befolkning_df = generate_population_df(befolkning_raw)
+    befolkning_district_df = generate_district_population_df(befolkning_df, ignore_districts=['16', '17'])
 
-    output_list = generate_output_list(landbakgrunn_df, data_points, n_district=10, n_total=15)
+    landbakgrunn_df = process_country_df(landbakgrunn_raw)
 
-    #print(json.dumps(output_list))
+    data_points = ['Innvandrer', 'Norskfødt med innvandrerforeldre', 'total']
+    input_df = pd.merge(landbakgrunn_df, befolkning_district_df, how='inner', on=['district', 'date'])
+    input_df = aggregator.add_ratios(input_df, data_points, ratio_of=['population'])
 
-    # _write_to_intermediate(historic_dataset_id, historic_version_id, historic_edition_id, low_education_historic)
-    # _write_to_intermediate(status_dataset_id, status_version_id, status_edition_id, low_education_status)
+    output_list_historic = generate_output_list(input_df, data_points, top_n=1, template_fun=generate_geo_obj_historic)
 
-def generate_output_list(df, data_points, n_district, n_total):
-    landbakgrunn_df = df
-    oslo_total_df = landbakgrunn_df.groupby(['date', 'Landbakgrunn']).sum().reset_index()
-    top_n_district = get_top_n_district(landbakgrunn_df, n_district)
-    top_n_total = get_top_n_total(oslo_total_df, n_total)
+    input_df_status = input_df[input_df['date'] == 2018]
+    output_list_status = generate_output_list(input_df_status, data_points, top_n=10, template_fun=generate_geo_obj_status)
+
+    _write_to_intermediate(historic_dataset_id, historic_version_id, historic_edition_id, output_list_historic)
+    _write_to_intermediate(status_dataset_id, status_version_id, status_edition_id, output_list_status)
+
+
+def generate_output_list(input_df, data_points, top_n, template_fun):
+    top_n_district = get_top_n_district(input_df, top_n)
 
     output_list = []
-    for district in landbakgrunn_df['Bydel'].unique():
+    for district in input_df['district'].unique():
         district_obj = {
             'district': district,
             'data': []
         }
-        district_df = landbakgrunn_df[landbakgrunn_df['Bydel'] == district]
+        district_df = input_df[input_df['district'] == district]
         for geography in top_n_district[district]:
             geo_df = district_df[district_df['Landbakgrunn'] == geography]
-            geo_obj = generate_geo_obj(geo_df, geography, data_points)
+            geo_obj = template_fun(geo_df, geography, data_points)
             district_obj['data'].append(geo_obj)
         output_list.append(district_obj)
 
-    oslo_total_obj = {
-        'district': 'Oslo i alt',
-        'data': []
-    }
-    for geography in top_n_total:
-        geo_df = oslo_total_df[oslo_total_df['Landbakgrunn'] == geography]
-        geo_obj = generate_geo_obj(geo_df, geography, data_points)
-        oslo_total_obj['data'].append(geo_obj)
-    output_list.append(oslo_total_obj)
     return output_list
 
-def generate_geo_obj(df, geography, data_points):
+
+def generate_geo_obj_status(df, geography, data_points):
+    series = {}
+    for value in df.to_dict('r'):
+        for data_point in data_points:
+            series[data_point] = {
+                'date': value['date'],
+                'value': value[data_point],
+                'ratio': value[f'{data_point}_ratio']
+            }
+    values = []
+    [values.append(series[data_point]) for data_point in data_points if series]
+    return {'geography': geography, 'values': values}
+
+
+def generate_geo_obj_historic(df, geography, data_points):
     series = list_to_time_series(data_points)
     for value in df.to_dict('r'):
         for data_point in data_points:
-            series[data_point].append({'date': value['date'], 'value': value[data_point]})
-    return {'geography': geography, 'values': series}
+            series[data_point].append({
+                'date': value['date'],
+                'value': value[data_point],
+                'ratio': value[f'{data_point}_ratio']})
+    values = []
+    [values.append(series[data_point]) for data_point in data_points]
+    return {'geography': geography, 'values': values}
+
 
 def list_to_time_series(data_points):
     d = {}
@@ -96,26 +111,42 @@ def list_to_time_series(data_points):
         d[data_point] = []
     return d
 
-def get_top_n_total(df, n):
-    total_df = df[df['date'] == 2018]
-    print(total_df['total'].sum())
-    total_df = total_df.nlargest(n, 'total')
-    return total_df['Landbakgrunn'].tolist()
 
-def population_df(df):
+def process_country_df(df):
+    data_points = ['Innvandrer', 'Norskfødt med innvandrerforeldre']
+    df['total'] = df[data_points].sum(axis=1)
+    data_points.append('total')
+    oslo_total_df = df.groupby(['date', 'Landbakgrunn']).sum().reset_index()
+    df['district'] = df['Bydel'].apply(util.get_district_id)
+    oslo_total_df['district'] = '00'
+    country_df = pd.concat((df, oslo_total_df), sort=False, ignore_index=True)
+    return country_df[['district', 'date', 'Landbakgrunn', 'Innvandrer', 'Norskfødt med innvandrerforeldre', 'total']]
+
+
+def generate_district_population_df(population_df, ignore_districts = []):
+    population_df = transform.add_district_id(population_df)
+    population_df = population_df[~population_df['district'].isin(ignore_districts)]
+    population_district_df = population_df.groupby(['district','date']).sum().reset_index()
+    oslo_total_df = population_district_df.groupby('date').sum().reset_index()
+    oslo_total_df['district'] = '00'
+    population_district_df = pd.concat((population_district_df, oslo_total_df), sort=False, ignore_index=True)
+    return population_district_df
+
+
+def generate_population_df(df):
     df = df[['delbydelid', 'Alder', 'Antall personer', 'Kjønn', 'date']]
     df = df[df['delbydelid'].notnull()]
     df = pivot_table(df, 'Kjønn', 'Antall personer')
-    df['total'] = df[1] + df[2]
-    df = df[['delbydelid', 'date', 'total']]
+    df['population'] = df[1] + df[2]
+    df = df[['delbydelid', 'date', 'population']]
     df = df.groupby(['delbydelid', 'date']).sum().reset_index()
     return df
 
 
 def get_top_n_district(df, n):
     top_n = {}
-    for district in df['Bydel'].unique():
-        district_df = df[df['Bydel'] == district]
+    for district in df['district'].unique():
+        district_df = df[df['district'] == district]
         district_df = district_df[district_df['date'] == 2018]
         district_df = district_df.nlargest(n, 'total')
         top_n[district] = district_df['Landbakgrunn'].tolist()
@@ -142,12 +173,16 @@ def _output_key(dataset_id, version_id, edition_id):
 
 
 def _write_to_intermediate(dataset_id, version_id, edition_id, output_list):
-    series = []
-    heading = "Personer mellom 30-59 år med lav utdanning"
+    series = [
+        {"heading": "Innvandrer", "subheading": ""},
+        {"heading": "Norskfødt med innvandrerforeldre", "subheading": ""},
+        {"heading": "Totalt", "subheading": ""}
+    ]
+    heading = "10 største innvandringsgrupper"
     output_key = _output_key(dataset_id, version_id, edition_id)
     common_aws.write_to_intermediate(output_key, output_list, heading, series)
 
-#start(None, None)
+
 if __name__ == '__main__':
     handle(
             {"bucket": "ok-origo-dataplatform-dev",
