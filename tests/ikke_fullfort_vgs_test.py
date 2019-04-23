1d99c746945037c0b3150ab5a2a4434bf00d3eec
Magnus Eide <magnus.eide@byr.oslo.kommune.no>
2019-04-23 13:13:54 +0200

    Check ratios in ikke_fullfort_vgs_test

--- tests/ikke_fullfort_vgs_test.py
+++ tests/ikke_fullfort_vgs_test.py
@@ -5,19 +5,17 @@ import tests.datasets_for_testing as test_data
 
 class Tester(unittest.TestCase):
     def test_ikke_fullfort_vgs_test(self):
-
         df_source = test_data.data_sets["df_ikke_vgs"]
-
         output_data = ikke_fullfort_vgs.data_processing(df_source)
 
         # Run a test on each type of template (depending on original to be perfect).
         self.assertAlmostEqual(
-            output_data["levekar_vgs_status"][0]["data"][0]["values"][0]["value"],
+            output_data["levekar_vgs_status"][0]["data"][0]["values"][0]["ratio"],
             0.2887,
             4,
         )
         self.assertAlmostEqual(
-            output_data["levekar_vgs_status"][0]["data"][1]["values"][0]["value"],
+            output_data["levekar_vgs_status"][0]["data"][1]["values"][0]["ratio"],
             0.2222,
             4,
         )
