bb81165d3374c1b4318152528119220b5ac91981
oyvind.nygard@knowit.no <oyvind.nygard@knowit.no>
2019-04-25 10:13:26 +0200

    Fix flake8 error

--- functions/landbakgrunn_innvandringsgrupper.py
+++ functions/landbakgrunn_innvandringsgrupper.py
@@ -6,7 +6,6 @@ import common.aws as common_aws
 import common.transform as transform
 import common.aggregate_dfs as aggregator
 import common.util as util
-from common.transform_output import generate_output_list
 
 os.environ["METADATA_API_URL"] = ""
 
