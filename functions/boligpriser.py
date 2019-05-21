bd6b11636d424e400a89a17fb3da018d31a35af1
Fredrik Vraalsen <fredrik.vraalsen@byr.oslo.kommune.no>
2019-05-21 13:57:27 +0200

    New event format from pipeline-router

--- functions/boligpriser.py
+++ functions/boligpriser.py
@@ -6,13 +6,14 @@ import common.transform_output
 
 def handler(event, context):
     """ Assuming we recieve a complete s3 key"""
-    s3_key = event["keys"]["boligpriser-blokkleiligheter"]
-    type_of_ds = event["type"]
-    start(s3_key, type_of_ds)
+    s3_key = event["input"]["boligpriser-blokkleiligheter"]
+    output_key = event["output"]
+    type_of_ds = event["config"]["type"]
+    start(s3_key, output_key, type_of_ds)
     return "OK"
 
 
-def start(key, type_of_ds):
+def start(key, output_key, type_of_ds):
     df = common.aws.read_from_s3(s3_key=key, date_column="aar", dtype={"bydel_id": object, "delbydel_id": object})
     df = df.rename(
         columns={
@@ -27,28 +28,24 @@ def start(key, type_of_ds):
     df = df[df.value.notnull()]
     df["district"].fillna("00", inplace=True)
 
-    print(df.to_string())
-
     status = common.transform.status(df)
     historic = common.transform.historic(df)
-    if type_of_ds == "historic":
-        create_ds("boligpriser-blokkleiligheter-historic", "c", *historic)
+    if type_of_ds == "historisk":
+        create_ds(output_key, "c", *historic)
     elif type_of_ds == "status":
-        create_ds("boligpriser-blokkleiligheter-status", "a", *status)
+        create_ds(output_key, "a", *status)
 
 
-def create_ds(dataset_id, template, df):
+def create_ds(output_key, template, df):
     heading = "Gjennomsnittpris (kr) pr kvm for blokkleilighet"
     series = [
         {"heading": "Gjennomsnittpris (kr) pr kvm for blokkleilighet", "subheading": ""}
     ]
 
-    print(df.to_string())
-
     # To json : convert df to list of json objects
     jsonl = common.transform_output.generate_output_list(df, template, ["value"])
     common.aws.write_to_intermediate(
-        output_key=f"processed/green/{dataset_id}/version=1/edition=20190520T144900/",
+        output_key=output_key,
         heading=heading,
         series=series,
         output_list=jsonl,
@@ -59,11 +56,13 @@ def create_ds(dataset_id, template, df):
 if __name__ == "__main__":
     handler(
         {
-            "bucket": "ok-origo-dataplatform-dev",
-            "keys": {
+            "input": {
                 "boligpriser-blokkleiligheter": "raw/green/boligpriser-blokkleiligheter/version=1/edition=20190520T114926/Boligpriser(2004-2017-v01).csv"
             },
-            "type": "status",
+            "output": "intermediate/green/boligpriser-blokkleiligheter-status/version=1/edition=20190520T114926/",
+            "config": {
+                "type": "status"
+            }
         },
         {},
     )
