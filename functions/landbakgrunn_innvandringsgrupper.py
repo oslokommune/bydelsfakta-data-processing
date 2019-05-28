9d3b760593a4bc1bcf400fe015404545302176cb
oyvind.nygard@knowit.no <oyvind.nygard@knowit.no>
2019-05-28 16:32:12 +0200

    Write to intermediate

--- functions/landbakgrunn_innvandringsgrupper.py
+++ functions/landbakgrunn_innvandringsgrupper.py
@@ -5,6 +5,7 @@ import pandas as pd
 import common.aws as common_aws
 import common.aggregate_dfs as aggregator
 import common.population_utils as population_utils
+from common.aggregateV2 import Aggregate
 
 os.environ["METADATA_API_URL"] = ""
 
@@ -12,27 +13,29 @@ pd.set_option("display.max_rows", 1000)
 
 
 def handle(event, context):
-    """ Assuming we recieve a complete s3 key"""
+    s3_key_landbakgrunn = event["input"]["landbakgrunn-storste-innvandringsgrupper"]
+    s3_key_befolkning = event["input"]["befolkning-etter-kjonn-og-alder"]
+    output_key = event["output"]
+    type_of_ds = event["config"]["type"]
     landbakgrunn_raw = common_aws.read_from_s3(
-        s3_key=event["input"]["landbakgrunn-storste-innvandringsgrupper"],
-        date_column="aar"
+        s3_key=s3_key_landbakgrunn, date_column="aar"
     )
     befolkning_raw = common_aws.read_from_s3(
-        s3_key=event["input"]["befolkning-delbydel.kjonn-alder"],
-        date_column="aar")
+        s3_key=s3_key_befolkning, date_column="aar"
+    )
 
     data_points = ["innvandrer", "norskfodt_med_innvandrerforeldre", "total"]
     input_df = generate_input_df(landbakgrunn_raw, befolkning_raw, data_points)
 
     output_list = []
-    if event['config']['type'] == "status":
+    if type_of_ds == "status":
         output_list = output_list_status(input_df, data_points, top_n=10)
-    elif event['config']['type'] == "historic":
+    elif type_of_ds == "historic":
         output_list = output_list_historic(input_df, data_points, top_n=10)
 
     if output_list:
-        _write_to_processed(output_list, event['output'])
-
+        common_aws.write_to_intermediate(output_key=output_key, output_list=output_list)
+        return f"Created {output_key}"
 
 
 def generate_input_df(landbakgrunn_raw, befolkning_raw, data_points):
@@ -40,24 +43,29 @@ def generate_input_df(landbakgrunn_raw, befolkning_raw, data_points):
     befolkning_df = population_utils.generate_population_df(befolkning_raw)
     # Ignoring Marka and Sentrum
     ignore_districts = ["16", "17"]
-    befolkning_district_df = population_by_district_df(
+    befolkning_district_df = population_district_only(
         befolkning_df, ignore_districts=ignore_districts
     )
 
     landbakgrunn_df = process_country_df(landbakgrunn_raw)
 
     input_df = pd.merge(
-        landbakgrunn_df, befolkning_district_df, how="inner", on=["bydel_id", "date", "bydel_navn"]
+        landbakgrunn_df,
+        befolkning_district_df,
+        how="inner",
+        on=["bydel_id", "date", "bydel_navn"],
     )
     input_df = aggregator.add_ratios(input_df, data_points, ratio_of=["population"])
 
     return input_df
+
 
 def output_list_historic(input_df, data_points, top_n):
     output_list = generate_output_list(
         input_df, data_points, top_n=top_n, template_fun=generate_geo_obj_historic
     )
     return output_list
+
 
 def output_list_status(input_df, data_points, top_n):
     input_df_status = input_df[input_df["date"] == input_df["date"].max()]
@@ -66,12 +74,15 @@ def output_list_status(input_df, data_points, top_n):
     )
     return output_list
 
+
 def generate_output_list(input_df, data_points, top_n, template_fun):
     top_n_countries = get_top_n_countries(input_df, top_n)
 
     district_list = [
         (x.bydel_id, x.bydel_navn)
-        for x in set(input_df.loc[:, ["bydel_id", "bydel_navn"]].itertuples(index=False))
+        for x in set(
+            input_df.loc[:, ["bydel_id", "bydel_navn"]].itertuples(index=False)
+        )
     ]
     output_list = []
     for district_id, district_name in district_list:
@@ -135,18 +146,14 @@ def process_country_df(df):
     ]
 
 
-def population_by_district_df(population_df, ignore_districts=[]):
+def population_district_only(population_df, ignore_districts=[]):
     population_df = population_df[~population_df["bydel_id"].isin(ignore_districts)]
-    population_district_df = (
-        population_df.groupby(["bydel_id", "bydel_navn", "date"])['population'].sum().reset_index()
-    )
-    oslo_total_df = population_district_df.groupby("date").sum().reset_index()
-    oslo_total_df["bydel_id"] = 00
-    oslo_total_df["bydel_navn"] = "Oslo i alt"
-    population_district_df = pd.concat(
-        (population_district_df, oslo_total_df), sort=False, ignore_index=True
-    )
-    return population_district_df
+    agg = {"population": "sum"}
+    population_aggregated_df = Aggregate(agg).aggregate(population_df)
+    population_district_only_df = population_aggregated_df[
+        population_aggregated_df["delbydel_id"].isnull()
+    ]
+    return population_district_only_df
 
 
 def get_top_n_countries(df, n):
@@ -159,22 +166,12 @@ def get_top_n_countries(df, n):
     return top_n
 
 
-def _write_to_processed(output_list, output_key):
-    series = [
-        {"heading": "innvandrer", "subheading": ""},
-        {"heading": "norskfodt_med_innvandrerforeldre", "subheading": ""},
-        {"heading": "Totalt", "subheading": ""},
-    ]
-    heading = "10 største innvandringsgrupper"
-    common_aws.write_to_intermediate(output_key, output_list, heading, series)
-
-
 if __name__ == "__main__":
     # handle(
     #     {
     #         "input": {
     #             "landbakgrunn-storste-innvandringsgrupper": "raw/green/landbakgrunn-storste-innvandringsgrupper/version=1/edition=20190523T211529/Landbakgrunn_storste_innvandringsgrupper(1.1.2008-1.1.2019-v01).csv",
-    #             "befolkning-delbydel.kjonn-alder": "raw/yellow/befolkning-etter-kjonn-og-alder/version=1/edition=20190523T211529/Befolkningen_etter_bydel_delbydel_kjonn_og_1-aars_aldersgrupper(1.1.2008-1.1.2019-v01).csv"
+    #             "befolkning-etter-kjonn-og-alder": "raw/yellow/befolkning-etter-kjonn-og-alder/version=1/edition=20190523T211529/Befolkningen_etter_bydel_delbydel_kjonn_og_1-aars_aldersgrupper(1.1.2008-1.1.2019-v01).csv"
     #         },
     #         "output": "s3/key/or/prefix",
     #         "config": {"type": "status"}
@@ -185,10 +182,10 @@ if __name__ == "__main__":
         {
             "input": {
                 "landbakgrunn-storste-innvandringsgrupper": "raw/green/landbakgrunn-storste-innvandringsgrupper/version=1/edition=20190523T211529/Landbakgrunn_storste_innvandringsgrupper(1.1.2008-1.1.2019-v01).csv",
-                "befolkning-delbydel.kjonn-alder": "raw/yellow/befolkning-etter-kjonn-og-alder/version=1/edition=20190523T211529/Befolkningen_etter_bydel_delbydel_kjonn_og_1-aars_aldersgrupper(1.1.2008-1.1.2019-v01).csv"
+                "befolkning-etter-kjonn-og-alder": "raw/yellow/befolkning-etter-kjonn-og-alder/version=1/edition=20190523T211529/Befolkningen_etter_bydel_delbydel_kjonn_og_1-aars_aldersgrupper(1.1.2008-1.1.2019-v01).csv",
             },
             "output": "s3/key/or/prefix",
-            "config": {"type": "historic"}
+            "config": {"type": "historic"},
         },
-        None
+        None,
     )
