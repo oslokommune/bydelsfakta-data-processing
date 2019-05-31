5b7ee195396d69408fe0972b84edbd85c85d122c
Magnus Eide <magnus.eide@byr.oslo.kommune.no>
2019-05-31 21:21:54 +0200

    Move docstring for population_utils into function

--- common/population_utils.py
+++ common/population_utils.py
@@ -1,15 +1,13 @@
-"""
-Purpose for this function is utility for datasets that needs to be enriched
-with the total populations in districts and subdistricts
-
-input dataframe must have columns: date', 'delbydel_id', 'delbydel_navn', 'bydel_id', 'bydel_navn', 'kjønn' and '0' to 'n'
-example: | date | delbydel_id | delbydel_navn | bydel_id | bydel_navn | kjønn | 0 | 1 | 2 | ..... | n |
-
-returns dataframe with columns: | date | delbydel_id | delbydel_navn | bydel_id | bydel_navn | population
-"""
-
-
 def generate_population_df(population_raw, min_age=0, max_age=200):
+    """
+    Purpose for this function is utility for datasets that needs to be enriched
+    with the total populations in districts and subdistricts
+
+    input dataframe must have columns: date', 'delbydel_id', 'delbydel_navn', 'bydel_id', 'bydel_navn', 'kjønn' and '0' to 'n'
+    example: | date | delbydel_id | delbydel_navn | bydel_id | bydel_navn | kjønn | 0 | 1 | 2 | ..... | n |
+
+    returns dataframe with columns: | date | delbydel_id | delbydel_navn | bydel_id | bydel_navn | population
+    """
 
     if max_age > int(population_raw.columns[-1]):
         max_age = int(population_raw.columns[-1])
