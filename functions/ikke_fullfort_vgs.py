eeb19f612b60ad336448a08ec4348881bfc06a30
tordsk <tord.kloster@knowit.no>
2019-05-31 18:09:53 +0200

    formatting

--- functions/ikke_fullfort_vgs.py
+++ functions/ikke_fullfort_vgs.py
@@ -10,7 +10,8 @@ from common.util import get_latest_edition_of
 column_names = ColumnNames()
 
 ikke_fullfort_antall = "antall_personer_ikke_fullfort_i_løpet_av_5_aar"
-ikke_fullfort_andel = 'andelen_som_ikke_har_fullfort_i_lopet_av_5_aar'
+ikke_fullfort_andel = "andelen_som_ikke_har_fullfort_i_lopet_av_5_aar"
+
 
 def read(s3_key):
     # Generate the dataframe we want to start aggregating on
@@ -18,12 +19,16 @@ def read(s3_key):
     df = df.rename(columns={ikke_fullfort_andel: f"{ikke_fullfort_antall}_ratio"})
     return df
 
+
 def process(df, type):
     metadata = Metadata(
-            heading="Personer som ikke har fullført vgs",
-            series=[
-                {"Heading": "Antall personer, ikke fullført i løpet av 5 år", "subheading": ""},
-            ]
+        heading="Personer som ikke har fullført vgs",
+        series=[
+            {
+                "Heading": "Antall personer, ikke fullført i løpet av 5 år",
+                "subheading": "",
+            }
+        ],
     )
     if type == "historisk":
         [df] = common.transform.historic(df)
@@ -35,13 +40,11 @@ def process(df, type):
         raise Exception("type must be status or historisk")
 
     output = Output(
-            df=df,
-            values=[ikke_fullfort_antall],
-            metadata=metadata,
-            template=template
+        df=df, values=[ikke_fullfort_antall], metadata=metadata, template=template
     ).generate_output()
 
     return output
+
 
 def write(output, s3_key):
     print(output)
@@ -61,13 +64,10 @@ def handler(event, context):
 
 if __name__ == "__main__":
     handler(
-            {"input": {
-                "ikke-fullfort-vgs": get_latest_edition_of(
-                        "ikke-fullfort-vgs"
-                )
-            },
-                "output": "intermediate/green/ikke-fullfort-vgs-status/version=1/edition=20110531T102550/",
-                "config": {"type": "status"},
-            },
-            {}
+        {
+            "input": {"ikke-fullfort-vgs": get_latest_edition_of("ikke-fullfort-vgs")},
+            "output": "intermediate/green/ikke-fullfort-vgs-status/version=1/edition=20110531T102550/",
+            "config": {"type": "status"},
+        },
+        {},
     )
