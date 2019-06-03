82f7069e75cea5e7999098aa0077ea17b1aaf802
oyvind.nygard@knowit.no <oyvind.nygard@knowit.no>
2019-06-03 14:33:05 +0200

    Updated lav_utdanning to work with new raw-data format

--- functions/lav_utdanning.py
+++ functions/lav_utdanning.py
@@ -1,119 +1,92 @@
-import os
-
-import pandas as pd
-
-import common.aws as common_aws
 import common.transform as transform
-import common.aggregate_dfs as aggregator
-from common.transform_output import generate_output_list
+import common.aws as common_aws
+from common.aggregateV2 import Aggregate
+from common.util import get_latest_edition_of
+from common.output import Output, Metadata
+from common.templates import TemplateA, TemplateB
 
-os.environ["METADATA_API_URL"] = ""
 
-s3_bucket = "ok-origo-dataplatform-dev"
-
-historic_dataset_id = "Lav_utdanning_historisk-qBXzf"
-historic_version_id = "1-tt4pE7NE"
-historic_edition_id = "EDITION-smQmD"
-status_dataset_id = "Lav_utdanning_status-fYdyK"
-status_version_id = "1-FswVHo7C"
-status_edition_id = "EDITION-H7XFn"
-
-pd.set_option("display.max_rows", 1000)
+graph_metadata = Metadata(
+    heading="Personer mellom 30-59 år med lav utdanning",
+    series=[{"heading": "Kun grunnskole eller ingen utdanning oppgitt", "subheading": ""}],
+)
 
 
 def handle(event, context):
     """ Assuming we recieve a complete s3 key"""
-    s3_key = event["keys"]["Lav_utdanning-Xn55b"]
-    bucket = event["bucket"]
-    start(bucket, s3_key)
-    return "OK"
+    s3_key_lav_utdanning = event["input"]["lav-utdanning"]
+    output_key = event["output"]
+    type_of_ds = event["config"]["type"]
 
+    data_point = "lav_utdanning"
 
-def start(bucket, key):
-    low_education_raw = common_aws.read_from_s3(s3_key=key, date_column="År")
-
-    data_points = ["low_education"]
-
-    input_df = generate_input_df(low_education_raw.copy())
-
-    low_education_historic = generate_output_list(
-        *transform.historic(input_df), template="b", data_points=data_points
-    )
-    low_education_status = generate_output_list(
-        *transform.status(input_df), template="a", data_points=data_points
+    lav_utdanning_raw = common_aws.read_from_s3(
+        s3_key=s3_key_lav_utdanning, date_column="aar"
     )
 
-    _write_to_intermediate(
-        historic_dataset_id,
-        historic_version_id,
-        historic_edition_id,
-        low_education_historic,
-    )
-    _write_to_intermediate(
-        status_dataset_id, status_version_id, status_edition_id, low_education_status
-    )
+    input_df = generate_input_df(lav_utdanning_raw, data_point)
+
+    output_list = []
+    if type_of_ds == "historisk":
+        output_list = output_historic(input_df, [data_point])
+
+    elif type_of_ds == "status":
+        output_list = output_status(input_df, [data_point])
+
+    if output_list:
+        common_aws.write_to_intermediate(output_key=output_key, output_list=output_list)
+        return f"Created {output_key}"
 
 
-def generate_input_df(df):
-    df = pivot_table(df, "Høyeste fullførte utdanning", "Antall personer")
-    with_district = transform.add_district_id(df)
-
-    value_cols = [
-        "Videregående skolenivå",
-        "Universitets- og høgskolenivå, kort",
-        "Universitets- og høgskolenivå, lang",
-        "Ingen utdanning/Uoppgitt utdanning",
-        "Grunnskole",
+def generate_input_df(lav_utdanning_df, data_point):
+    education_categories = [
+        'ingen_utdanning_uoppgitt',
+        'grunnskole',
+        'videregaende',
+        'universitet_hogskole_kort',
+        'universitet_hogskole_lang'
     ]
-    df_aggregated = aggregator.aggregate_from_subdistricts(
-        with_district, _aggregations(value_cols)
-    )
 
-    data_point = "low_education"
-    df_aggregated[data_point] = (
-        df_aggregated["Ingen utdanning/Uoppgitt utdanning"]
-        + df_aggregated["Grunnskole"]
-    )
+    lav_utdanning_df['total'] = lav_utdanning_df[education_categories].sum(axis=1)
+    lav_utdanning_df[data_point] = lav_utdanning_df[['ingen_utdanning_uoppgitt', 'grunnskole']].sum(axis=1)
 
-    input_df = aggregator.add_ratios(df_aggregated, [data_point], value_cols)
+    aggregations = {
+        data_point: 'sum',
+        'total': 'sum'
+    }
+    aggregator = Aggregate(aggregations)
+    input_df = aggregator.aggregate(lav_utdanning_df)
+
+    input_df = aggregator.add_ratios(input_df, data_points=[data_point], ratio_of=['total'])
     return input_df
 
 
-def pivot_table(df, pivot_column, value_column):
-    key_columns = list(
-        filter(lambda x: x not in [pivot_column, value_column], list(df))
-    )
-    df_pivot = pd.concat(
-        (df[key_columns], df.pivot(columns=pivot_column, values=value_column)), axis=1
-    )
-    return df_pivot.groupby(key_columns).sum().reset_index()
+def output_historic(input_df, data_points):
+    [input_df] = transform.historic(input_df)
+    output = Output(
+        values=data_points, df=input_df, metadata=graph_metadata, template=TemplateB()
+    ).generate_output()
+
+    return output
 
 
-def _aggregations(data_points):
-    return [
-        {"data_points": data_point, "agg_func": "sum"} for data_point in data_points
-    ]
+def output_status(input_df, data_points):
+    [input_df] = transform.status(input_df)
+    output = Output(
+        values=data_points, df=input_df, metadata=graph_metadata, template=TemplateA()
+    ).generate_output()
+    return output
 
 
-def _output_key(dataset_id, version_id, edition_id):
-    return f"processed/green/{dataset_id}/version={version_id}/edition={edition_id}/"
-
-
-def _write_to_intermediate(dataset_id, version_id, edition_id, output_list):
-    series = []
-    heading = "Personer mellom 30-59 år med lav utdanning"
-    output_key = _output_key(dataset_id, version_id, edition_id)
-    common_aws.write_to_intermediate(output_key, output_list, heading, series)
-
-
-# start(None, None)
 if __name__ == "__main__":
+    lav_utdanning_s3_key = get_latest_edition_of('lav-utdanning')
     handle(
         {
-            "bucket": "ok-origo-dataplatform-dev",
-            "keys": {
-                "Lav_utdanning-Xn55b": "raw/green/Lav_utdanning-Xn55b/version=1-pYXX8pg4/edition=EDITION-7ofpj/Lav_utdanning(1.1.2008-1.1.2018-v01).csv"
+            "input": {
+                "lav-utdanning": lav_utdanning_s3_key
             },
+            "output": "s3/key/or/prefix",
+            "config": {"type": "historisk"}
         },
-        {},
+        None
     )
