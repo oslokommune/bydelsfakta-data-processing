15db611f21d9ad9c146a4ecaed59450cd3baad1f
oyvind.nygard@knowit.no <oyvind.nygard@knowit.no>
2019-06-11 10:24:27 +0200

    Using population as ratio_of

--- functions/botid_ikke_vestlige.py
+++ functions/botid_ikke_vestlige.py
@@ -1,13 +1,13 @@
-from common.aws import read_from_s3, write_to_intermediate
-from common.templates import TemplateA, TemplateB
-import common.transform
-import common.util
-import common.transform_output
-from common.aggregateV2 import Aggregate
-
 import pandas as pd
 
+import common.transform
+import common.util as util
+import common.transform_output
+from common.aws import read_from_s3, write_to_intermediate
+from common.templates import TemplateA, TemplateB
+from common.aggregateV2 import Aggregate
 from common.output import Metadata, Output
+from common.population_utils import generate_population_df
 
 pd.set_option("display.max_rows", 1000)
 
@@ -24,57 +24,66 @@ graph_metadata = Metadata(
 
 def handler(event, context):
     """ Assuming we recieve a complete s3 key"""
-    s3_key = event["input"]["botid-ikke-vestlige"]
+    s3_key_botid = event["input"]["botid-ikke-vestlige"]
+    s3_key_befolkning = event["input"]["befolkning-etter-kjonn-og-alder"]
     output_key = event["output"]
     type_of_ds = event["config"]["type"]
-    start(s3_key, output_key, type_of_ds)
+    start(s3_key_botid, s3_key_befolkning, output_key, type_of_ds)
     return "OK"
 
 
-def start(key, output_key, type_of_ds):
-    df = read_from_s3(
-        s3_key=key, date_column="aar", dtype={"bydel_id": object, "delbydel_id": object}
-    )
-    df = df.drop(columns=["norge"])
-    df["total"] = (
-        df["asia_afrika_latin_amerika_og_ost_europa_utenfor_eu"]
-        + df["vest_europa_usa_canada_australia_og_new_zealand"]
-        + df["ost_europeiske_eu_land"]
+def start(s3_key_botid, s3_key_befolkning, output_key, type_of_ds):
+    botid_ikke_vestlige_raw = read_from_s3(
+        s3_key=s3_key_botid, date_column="aar"
     )
 
-    df = pivot_table(df, "botid", "total")
-
-    df = df.rename(
-        columns={
-            "Innvandrer, lang botid (>5 år)": "innvandrer_lang",
-            "Innvandrer, kort botid (<=5 år)": "innvandrer_kort",
-            "Øvrige befolkning": "ovrig_befolkning",
-        }
-    )
-    df = (
-        df.groupby(["delbydel_id", "date", "bydel_id", "delbydel_navn", "bydel_navn"])
-        .sum()
-        .reset_index()
-    )
-    value = "innvandrer_kort"
-
-    df["totalt_beboere"] = (
-        df["innvandrer_lang"] + df["ovrig_befolkning"] + df["innvandrer_kort"]
-    )
-    agg = {value: "sum", "totalt_beboere": "sum"}
-    df = Aggregate(agg).aggregate(df=df)
-    df = Aggregate(agg).add_ratios(
-        df=df, data_points=[value], ratio_of=["totalt_beboere"]
+    befolkning_raw = read_from_s3(
+        s3_key=s3_key_befolkning, date_column='aar'
     )
 
-    status = common.transform.status(df)
-    historic = common.transform.historic(df)
+    data_point = "ikke_vestlig_kort"
+
+    df = generate_ikke_vestlig_innvandrer_kort_botid_df(botid_ikke_vestlige_raw, befolkning_raw, data_point=data_point)
+
     if type_of_ds == "historisk":
+        historic = common.transform.historic(df)
         create_ds(
-            output_key, TemplateB(), ["innvandrer_kort"], graph_metadata, *historic
+            output_key, TemplateB(), [data_point], graph_metadata, *historic
         )
     elif type_of_ds == "status":
-        create_ds(output_key, TemplateA(), ["innvandrer_kort"], graph_metadata, *status)
+        status = common.transform.status(df)
+        create_ds(output_key, TemplateA(), [data_point], graph_metadata, *status)
+
+
+def generate_ikke_vestlig_innvandrer_kort_botid_df(
+    botid_ikke_vestlige_raw, befolkning_raw, data_point
+):
+
+    kort_botid = "Innvandrer, kort botid (<=5 år)"
+    ikke_vestlig = "asia_afrika_latin_amerika_og_ost_europa_utenfor_eu"
+
+    df = botid_ikke_vestlige_raw.drop(columns=["norge"])
+    df = pivot_table(df, "botid", ikke_vestlig)
+    df[data_point] = df[kort_botid]
+    df = Aggregate({data_point: "sum"}).aggregate(df)
+
+    population_df = generate_population_df(befolkning_raw)
+    population_district_df = Aggregate({"population": "sum"}).aggregate(
+        df=population_df
+    )
+
+    df = pd.merge(
+        df,
+        population_district_df[["date", "bydel_id", "delbydel_id", "population"]],
+        how="inner",
+        on=["bydel_id", "date", "delbydel_id"],
+    )
+
+    df = Aggregate({}).add_ratios(
+        df=df, data_points=[data_point], ratio_of=["population"]
+    )
+
+    return df
 
 
 def pivot_table(df, pivot_column, value_column):
@@ -98,7 +107,8 @@ if __name__ == "__main__":
     handler(
         {
             "input": {
-                "botid-ikke-vestlige": "raw/green/botid-ikke-vestlige/version=1/edition=20190525T183610/Botid_ikke_vestlige(1.1.2008-1.1.2019-v01).csv"
+                "botid-ikke-vestlige": util.get_latest_edition_of('botid-ikke-vestlige'),
+                "befolkning-etter-kjonn-og-alder": util.get_latest_edition_of("befolkning-etter-kjonn-og-alder", confidentiality='yellow')
             },
             "output": "intermediate/green/levekar-innvandrere-ikke-vestlige-kort-status/version=1/edition=20190525T183610/",
             "config": {"type": "status"},
