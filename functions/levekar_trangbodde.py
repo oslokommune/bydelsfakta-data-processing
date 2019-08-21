96c336035c268abc7d26620882424ab328a289a5
Aulon Mujaj <aulon.mujaj@gmail.com>
2019-08-21 12:58:40 +0200

    changed what trangbodde uses as a source, removed befolkning, only uses trangbodde-csv file

--- functions/levekar_trangbodde.py
+++ functions/levekar_trangbodde.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 import common.aws as common_aws
-from common.aggregateV2 import Aggregate
-import common.population_utils as population_utils
 import common.transform as transform
 from common.output import Output, Metadata
 from common.templates import TemplateA, TemplateB
+from common.util import get_latest_edition_of
+
 
 pd.set_option("display.max_rows", 1000)
 
@@ -16,27 +16,24 @@ graph_metadata = Metadata(
 
 def handle(event, context):
     s3_key_trangbodde = event["input"]["trangbodde"]
-    s3_key_befolkning = event["input"]["befolkning-etter-kjonn-og-alder"]
     output_key = event["output"]
     type_of_ds = event["config"]["type"]
 
     trangbodde_raw = common_aws.read_from_s3(
         s3_key=s3_key_trangbodde, date_column="aar"
     )
-    befolkning_raw = common_aws.read_from_s3(
-        s3_key=s3_key_befolkning, date_column="aar"
+    datapoint = "antall_som_bor_trangt"
+
+    trangbodde_raw["antall_som_bor_trangt_ratio"] = (
+        trangbodde_raw["andel_som_bor_trangt"] / 100
     )
-
-    datapoint = "antall_trangbodde"
-
-    input_df = generate_input_df(trangbodde_raw, befolkning_raw, datapoint)
 
     output_list = []
     if type_of_ds == "historisk":
-        output_list = output_historic(input_df, [datapoint])
+        output_list = output_historic(trangbodde_raw, [datapoint])
 
     elif type_of_ds == "status":
-        output_list = output_status(input_df, [datapoint])
+        output_list = output_status(trangbodde_raw, [datapoint])
 
     if output_list:
         common_aws.write_to_intermediate(output_key=output_key, output_list=output_list)
@@ -44,37 +41,6 @@ def handle(event, context):
 
     else:
         raise Exception("No data in outputlist")
-
-
-def generate_input_df(trangbodde_raw, befolkning_raw, data_point):
-    population_df = population_utils.generate_population_df(befolkning_raw)
-
-    agg = {"population": "sum"}
-    population_district_df = Aggregate(agg).aggregate(df=population_df)
-
-    input_df = pd.merge(
-        trangbodde_raw,
-        population_district_df,
-        how="inner",
-        on=["bydel_id", "date", "delbydel_id"],
-    ).rename(columns={"bydel_navn_x": "bydel_navn", "delbydel_navn_x": "delbydel_navn"})
-    input_df[f"{data_point}_ratio"] = input_df["andel_som_bor_trangt"] / 100
-    input_df[data_point] = input_df["population"] * input_df[f"{data_point}_ratio"]
-
-    # Exclude Marka, Sentrum and Uten registrert adresse
-    input_df = input_df[~input_df["bydel_id"].isin(["16", "17", "99"])]
-
-    return input_df[
-        [
-            "date",
-            "bydel_id",
-            "bydel_navn",
-            "delbydel_id",
-            "delbydel_navn",
-            data_point,
-            f"{data_point}_ratio",
-        ]
-    ]
 
 
 def output_historic(input_df, data_points):
@@ -97,11 +63,8 @@ def output_status(input_df, data_points):
 if __name__ == "__main__":
     handle(
         {
-            "input": {
-                "trangbodde": "raw/green/trangbodde/version=1/edition=20190529T121303/Trangbodde(1.1.2015-1.1.2017-v01).csv",
-                "befolkning-etter-kjonn-og-alder": "raw/yellow/befolkning-etter-kjonn-og-alder/version=1/edition=20190523T211529/Befolkningen_etter_bydel_delbydel_kjonn_og_1-aars_aldersgrupper(1.1.2008-1.1.2019-v01).csv",
-            },
-            "output": "s3/key/or/prefix",
+            "input": {"trangbodde": get_latest_edition_of("trangbodde")},
+            "output": "intermediate/green/levekar-trangbodde-status/version=1/edition=20190821T124040/",
             "config": {"type": "status"},
         },
         None,
